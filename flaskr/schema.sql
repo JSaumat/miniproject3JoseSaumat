f190a3d5d254176a5d737d83e3d1657c39574727
JSaumat <j_saumat@mail.fhsu.edu>
2025-03-14 21:16:23 -0700

    Added first name and last name to registration page. Also added firstname next to time stamps.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -5,7 +5,9 @@ DROP TABLE IF EXISTS post;
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  firstname TEXT NOT NULL,
+  lastname TEXT NOT NULL
 );
 
 -- Creates a table for the posts on the forums
